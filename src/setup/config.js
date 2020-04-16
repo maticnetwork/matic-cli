95f12e410969448cbd577de1f55607bbd85bcc9d
Jaynti Kanani <jdkanani@gmail.com>
2020-04-16 15:22:13 +0530

    new: add config file

--- src/setup/config.js
+++ src/setup/config.js
@@ -1,3 +1,40 @@
-class Config {
+import inquirer from 'inquirer';
+import fs from 'fs-extra';
+
+const defaultConfigFileName = 'config.json'
+
+export default class Config {
   constructor() { }
+
+  toJSON() { }
+}
+
+export async function loadConfig(targetDirectory, configFileName) {
+  const configFile = path.join(targetDirectory, configFileName || defaultConfigFileName)
+
+  const hasConfigFile = await fs.exists(configFile)
+  if (hasConfigFile) {
+    const { override } = await inquirer.prompt({
+      type: 'confirm',
+      name: 'override',
+      message: 'Configuration found in this directory. Do you want to override?'
+    });
+
+    if (override) {
+      await fs.remove(configFile)
+    } else {
+      process.exit(0) // exit from the process
+    }
+  }
+
+  // create new config
+  const config = new Config()
+  config.fileName = configFileName || defaultConfigFileName
+  config.targetDirectory = targetDirectory
+}
+
+export async function saveConfig(config, targetDirectory) {
+  const configFile = path.join(targetDirectory, config.fileName || defaultConfigFileName)
+  const data = JSON.stringify(config.toJSON(), null, 2)
+  return fs.writeFileSync(configFile, data, { mode: 0o755 })
 }
--- src/setup/localnet/index.js
+++ src/setup/localnet/index.js
@@ -6,7 +6,8 @@ import { getGenesisContractTasks, printGenesisPath } from '../genesis'
 import { getHeimdallTasks, printHeimdallPaths, printAccount } from '../heimdall'
 import { getGanacheTasks, printGanacheDBPaths } from '../ganache'
 import { getBorTasks, printBorDetails } from '../bor'
-import { getChainIds, printDependencyInstructions } from '../helper'
+import { getChainIds, getKeystoreDetails, printDependencyInstructions } from '../helper'
+import { getWalletFromPrivateKey } from '../../utils'
 
 async function setupLocalnet(options) {
   const tasks = new Listr(
@@ -57,14 +58,24 @@ async function setupLocalnet(options) {
 export default async function () {
   await printDependencyInstructions()
 
-  // get answers
-  const answers = await getChainIds()
-
   // options
   let options = {
-    ...answers,
     targetDirectory: process.cwd(),
   };
+
+  // get answers
+  const answers = await getChainIds(options)
+  options = Object.assign(options, answers)
+
+  // get private key, keystore password and wallet
+  const keystoreDetails = await getKeystoreDetails()
+  options = Object.assign(options, keystoreDetails)
+
+  // wallet
+  const wallet = await getWalletFromPrivateKey(options.privateKey)
+
+  // set genesis addresses
+  options.genesisAddresses = [wallet.address]
 
   // start setup
   await setupLocalnet(options)
