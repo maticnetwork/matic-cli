f9c34d60bd46bc8bd9b253c95636c7f585a31bb5
Arpit Temani <temaniarpit27@gmail.com>
2022-06-27 12:44:06 +0530

    format repo

--- src/setup/localnet/index.js
+++ src/setup/localnet/index.js
@@ -1,98 +1,102 @@
-import Listr from 'listr'
-import chalk from 'chalk'
-import path from 'path'
-import fs from 'fs-extra'
+import Listr from "listr";
+import chalk from "chalk";
+import path from "path";
+import fs from "fs-extra";
 
-import { printDependencyInstructions, getDefaultBranch } from '../helper'
-import { loadConfig } from '../config'
+import { printDependencyInstructions, getDefaultBranch } from "../helper";
+import { loadConfig } from "../config";
 
-import { Genesis } from '../genesis'
-import { Heimdall } from '../heimdall'
-import { Ganache } from '../ganache'
-import { Bor } from '../bor'
-import { processTemplateFiles } from '../../lib/utils'
+import { Genesis } from "../genesis";
+import { Heimdall } from "../heimdall";
+import { Ganache } from "../ganache";
+import { Bor } from "../bor";
+import { processTemplateFiles } from "../../lib/utils";
 
 async function setupLocalnet(config) {
-  const ganache = new Ganache(config, { contractsBranch: config.contractsBranch })
-  const bor = new Bor(config, { repositoryBranch: config.borBranch })
-  const heimdall = new Heimdall(config, { repositoryBranch: config.heimdallBranch })
-  const genesis = new Genesis(config, { repositoryBranch: 'master' })
+  const ganache = new Ganache(config, {
+    contractsBranch: config.contractsBranch,
+  });
+  const bor = new Bor(config, { repositoryBranch: config.borBranch });
+  const heimdall = new Heimdall(config, {
+    repositoryBranch: config.heimdallBranch,
+  });
+  const genesis = new Genesis(config, { repositoryBranch: "master" });
 
   const tasks = new Listr(
     [
       {
         title: ganache.taskTitle,
         task: () => {
-          return ganache.getTasks()
-        }
+          return ganache.getTasks();
+        },
       },
       {
         title: heimdall.taskTitle,
         task: () => {
-          return heimdall.getTasks()
-        }
+          return heimdall.getTasks();
+        },
       },
       {
         title: genesis.taskTitle,
         task: () => {
-          return genesis.getTasks()
-        }
+          return genesis.getTasks();
+        },
       },
       {
         title: bor.taskTitle,
         task: () => {
-          return bor.getTasks()
-        }
+          return bor.getTasks();
+        },
       },
       {
-        title: 'Process scripts',
+        title: "Process scripts",
         task: async () => {
           const templateDir = path.resolve(
             new URL(import.meta.url).pathname,
-            '../templates'
-          )
+            "../templates"
+          );
 
           // copy all templates to target directory
-          await fs.copy(templateDir, config.targetDirectory)
+          await fs.copy(templateDir, config.targetDirectory);
 
           // process all njk templates
-          await processTemplateFiles(config.targetDirectory, { obj: this })
-        }
-      }
+          await processTemplateFiles(config.targetDirectory, { obj: this });
+        },
+      },
     ],
     {
-      exitOnError: true
+      exitOnError: true,
     }
-  )
+  );
 
-  await tasks.run()
-  console.log('%s Localnet ready', chalk.green.bold('DONE'))
+  await tasks.run();
+  console.log("%s Localnet ready", chalk.green.bold("DONE"));
 
   // print details
-  await config.print()
+  await config.print();
 
-  await genesis.print()
-  await heimdall.print()
-  await genesis.print()
-  await bor.print()
+  await genesis.print();
+  await heimdall.print();
+  await genesis.print();
+  await bor.print();
 }
 
 export default async function (command) {
-  await printDependencyInstructions()
+  await printDependencyInstructions();
 
   // configuration
   await loadConfig({
-    targetDirectory: command.parent.directory, 
+    targetDirectory: command.parent.directory,
     fileName: command.parent.config,
-    interactive: command.parent.interactive
-  })
-  await config.loadChainIds()
-  await config.loadAccounts()
+    interactive: command.parent.interactive,
+  });
+  await config.loadChainIds();
+  await config.loadAccounts();
 
   // load branch
-  const answers = await getDefaultBranch(config)
-  config.set(answers)
+  const answers = await getDefaultBranch(config);
+  config.set(answers);
 
   // start setup
-  await setupLocalnet(config)
+  await setupLocalnet(config);
 }
