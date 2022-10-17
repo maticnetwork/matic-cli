ee8f01e71fe752972dcba664ff15195726151779
marcello33 <marcelloardizzone@hotmail.it>
2022-10-17 09:42:14 +0200

    dev: chg: use default branches

--- src/setup/contracts/index.js
+++ src/setup/contracts/index.js
@@ -11,7 +11,7 @@ export class Contracts {
         this.config = config;
 
         this.repositoryName = "contracts";
-        this.repositoryBranch = "node-update";
+        this.repositoryBranch = options.repositoryBranch || "master";
         this.repositoryUrl =
             options.repositoryUrl || "https://github.com/maticnetwork/contracts";
     }
--- src/setup/genesis/index.js
+++ src/setup/genesis/index.js
@@ -20,7 +20,7 @@ export class Genesis {
         this.config = config;
 
         this.repositoryName = this.name;
-        this.repositoryBranch = "jesse/dependency-update";
+        this.repositoryBranch = options.repositoryBranch || "master";
         this.repositoryUrl =
             options.repositoryUrl ||
             "https://github.com/maticnetwork/genesis-contracts";
