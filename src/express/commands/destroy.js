97063393a1d200108084db5a62a28e542d0e355d
marcello33 <marcelloardizzone@hotmail.it>
2022-12-02 08:39:11 +0100

    dev: fix: typo

--- src/express/commands/destroy.js
+++ src/express/commands/destroy.js
@@ -5,7 +5,7 @@ const shell = require("shelljs");
 export async function terraformDestroy() {
     console.log("📍Executing terraform destroy...")
     require('dotenv').config({path: `${process.cwd()}/.env`})
-    shell.exec(`terraform dumb -auto-approve`, {
+    shell.exec(`terraform destroy -auto-approve`, {
         env: {
             ...process.env,
         }
