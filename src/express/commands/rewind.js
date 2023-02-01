a262b33ee565c2e56d16c5315ee51049e854b133
Nikhil Agarwal <nikhil.agarwal@iiitb.org>
2023-02-01 11:12:39 +0530

    chg: minor format fixes in console logs

--- src/express/commands/rewind.js
+++ src/express/commands/rewind.js
@@ -6,10 +6,10 @@ const {
 } = require('../common/remote-worker')
 
 export async function rewind(num) {
-  //   num = number of blocks to rewind
+  // num = number of blocks to rewind
   if (num > 128) {
     console.log(
-      '📍number of blocks to rewind should should be less than equal to 128, setting to 128'
+      '📍 Number of blocks to rewind should be less than or equal to 128, setting to 128'
     )
     num = 128
   }
@@ -24,16 +24,16 @@ export async function rewind(num) {
   const borHosts = splitToArray(doc.devnetBorHosts.toString())
 
   if (doc.devnetBorHosts.length > 0) {
-    console.log('📍Monitoring the first node', doc.devnetBorHosts[0])
+    console.log('📍 Monitoring the first node', doc.devnetBorHosts[0])
   } else {
-    console.log('📍No nodes to monitor, please check your configs! Exiting...')
+    console.log('📍 No nodes to monitor, please check your configs! Exiting...')
     process.exit(1)
   }
 
   const ip = `${borUsers[0]}@${borHosts[0]}`
 
   const getBlockNumberCommand =
-    '/home/ubuntu/go/bin/bor attach ~/.bor/data/bor.ipc --exec "eth.blockNumber"'
+    '~/go/bin/bor attach ~/.bor/data/bor.ipc --exec "eth.blockNumber"'
 
   const intitalBlockNumber = await runSshCommandWithReturn(
     ip,
@@ -41,10 +41,10 @@ export async function rewind(num) {
     maxRetries
   )
   console.log(
-    `⏪ rewinding chain by ${num} blocks, \n⛓ current block number: ${intitalBlockNumber}`
+    `📍 rewinding chain by ${num} blocks, \n📍 current block number: ${intitalBlockNumber}`
   )
 
-  const rewindCommand = `/home/ubuntu/go/bin/bor attach ~/.bor/data/bor.ipc --exec "debug.setHead(web3.toHex(${intitalBlockNumber} - ${num}))"`
+  const rewindCommand = `~/go/bin/bor attach ~/.bor/data/bor.ipc --exec "debug.setHead(web3.toHex(${intitalBlockNumber} - ${num}))"`
   await runSshCommand(ip, rewindCommand, maxRetries)
 
   const restartCommand = 'sudo service bor restart'
@@ -56,14 +56,14 @@ export async function rewind(num) {
     maxRetries
   )
   console.log(
-    `⏪ rewinded chain by ${
+    `📍 rewinded chain by ${
       intitalBlockNumber - rewindedBlockNumber
-    } blocks, \n⛓ current block number ${rewindedBlockNumber}`
+    } blocks, \n📍 current block number ${rewindedBlockNumber}`
   )
 
   console.log(
     'NOTE: minor difference in block number is expected due to small block time'
   )
 
-  console.log('📍Done! Exiting...')
+  console.log('📍 Done! Exiting...')
 }
