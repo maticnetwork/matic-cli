b5848f678748b32ad1d52cd355be7313c2d36cd4
Mohit Dhaundiyal <mohit.dhaundiyal@searce.com>
2023-06-29 10:15:53 +0530

    updated key pair changes

--- src/express/commands/aws-keypair-add.js
+++ src/express/commands/aws-keypair-add.js
@@ -2,6 +2,7 @@ import { loadDevnetConfig, splitToArray } from '../common/config-utils'
 import { maxRetries, runSshCommand } from '../common/remote-worker'
 
 const shell = require('shelljs')
+const fs = require('fs');
 export async function awsKeypairAdd() {
   require('dotenv').config({ path: `${process.cwd()}/.env` })
   const devnetType =
@@ -17,63 +18,102 @@ export async function awsKeypairAdd() {
     shell.exec(
       `aws ec2 create-key-pair --key-name ${keyName} --key-type rsa --key-format pem --query "KeyMaterial" --output text > ${keyName}.pem`
     )
+
+    if (shell.error() !== null) {
+      console.log('📍 Creation of aws key-pair failed')
+      process.exit(1)
+    } else {
+      console.log(`📍 Creation of aws key-pair ${keyName} successful`)
+    }
+    console.log(`📍 Assigning proper permission to ${keyName} ...`)
+    shell.exec(`chmod 700 ${keyName}.pem`)
+    if (shell.error() !== null) {
+      console.log(`📍 Granting permissions to ${keyName} failed`)
+      process.exit(1)
+    }
+    const output = shell.exec(
+      `aws ec2 describe-key-pairs --key-names ${keyName} --include-public-key --output json`
+    )
+    const pubKey = JSON.parse(output).KeyPairs[0].PublicKey.toString()
+
+    const totalHosts = []
+    const totalUsers = []
+    const nodeIps = []
+    if (doc.devnetBorHosts) {
+      totalHosts.push(...splitToArray(doc.devnetBorHosts.toString()))
+    }
+    if (doc.devnetErigonHosts) {
+      totalHosts.push(...splitToArray(doc.devnetErigonHosts.toString()))
+    }
+
+    if (doc.devnetBorUsers) {
+      totalUsers.push(...splitToArray(doc.devnetBorUsers.toString()))
+    }
+    if (doc.devnetErigonUsers) {
+      totalUsers.push(...splitToArray(doc.devnetErigonUsers.toString()))
+    }
+    let ip
+
+    for (let i = 0; i < totalHosts.length; i++) {
+      ip = `${totalUsers[i]}@${totalHosts[i]}`
+      nodeIps.push(ip)
+    }
+
+    const addKeyTasks = nodeIps.map(async (ip) => {
+      console.log(`📍 Adding ssh pubKey for ${keyName} to host ` + ip)
+      const command = `echo ${pubKey} >> ~/.ssh/authorized_keys`
+      await runSshCommand(ip, command, maxRetries)
+    })
+
+    await Promise.all(addKeyTasks)
+    console.log(`📍 Successfully added ${keyName} to all machines of the devnet`)
+    console.log(
+      `🔑 You can now share ${keyName}.pem with other devs - on a secure channel - to let them access the devnet`
+    )
+    console.log(
+      `🚨 Do not forget to destroy the key when no longer needed, using the command "../../bin/express-cli --aws-key-des ${keyName}"`
+    )
   } else if (cloud == 'gcp') {
-    console.log('📍 Not supported for gcp at the moment')
-    return
-  }
+    const user = doc.ethHostUser.toString()
+    const project = doc.instancesIds[0].split('/')[1].toString()
+    const zone = doc.instancesIds[0].split('/')[3].toString()
+    const instances = doc.instancesIds.map(x => x.split('/').at(-1)).toString().replace(/,/g, ' ').split(" ")
+    const keyFilePath = `${keyName}.pem.pub`;
 
-  if (shell.error() !== null) {
-    console.log('📍 Creation of aws key-pair failed')
-    process.exit(1)
-  } else {
-    console.log(`📍 Creation of aws key-pair ${keyName} successful`)
-  }
-  console.log(`📍 Assigning proper permission to ${keyName} ...`)
-  shell.exec(`chmod 700 ${keyName}.pem`)
-  if (shell.error() !== null) {
-    console.log(`📍 Granting permissions to ${keyName} failed`)
-    process.exit(1)
-  }
-  const output = shell.exec(
-    `aws ec2 describe-key-pairs --key-names ${keyName} --include-public-key --output json`
-  )
-  const pubKey = JSON.parse(output).KeyPairs[0].PublicKey.toString()
+    console.log('📍 Generating gcp key-pair...')
+    shell.exec(
+      `ssh-keygen -t rsa -q -N '' -C ${keyName} -f ${keyName}.pem`
+    )
 
-  const totalHosts = []
-  const totalUsers = []
-  const nodeIps = []
-  if (doc.devnetBorHosts) {
-    totalHosts.push(...splitToArray(doc.devnetBorHosts.toString()))
-  }
-  if (doc.devnetErigonHosts) {
-    totalHosts.push(...splitToArray(doc.devnetErigonHosts.toString()))
-  }
+    await Promise.all(instances.map(async (instance) => {
+      const existing_keys = shell.exec(
+        `gcloud compute instances describe ${instance} --project=${project} --zone=${zone} --format='value(metadata.ssh-keys)'`
+      )
 
-  if (doc.devnetBorUsers) {
-    totalUsers.push(...splitToArray(doc.devnetBorUsers.toString()))
-  }
-  if (doc.devnetErigonUsers) {
-    totalUsers.push(...splitToArray(doc.devnetErigonUsers.toString()))
-  }
-  let ip
+      const new_public_key = await new Promise((resolve, reject) => {
+        fs.readFile(keyFilePath, 'utf-8', (error, data) => {
+          if (error) {
+            console.error('Error reading file:', error);
+            reject(error);
+          } else {
+            resolve(data);
+          }
+        });
+      });
 
-  for (let i = 0; i < totalHosts.length; i++) {
-    ip = `${totalUsers[i]}@${totalHosts[i]}`
-    nodeIps.push(ip)
+      const new_keys = new_public_key + existing_keys;
+
+      shell.exec(
+        `gcloud compute instances add-metadata ${instance} --metadata ssh-keys="${user}:${new_keys}" --project=${project} --zone=${zone}`
+      )
+    }));
+    
+    console.log(`📍 Successfully added ${keyName} to all machines of the devnet`)
+    console.log(
+      `🔑 You can now share ${keyName}.pem with other devs - on a secure channel - to let them access the devnet`
+    )
+    console.log(
+      `🚨 Do not forget to destroy the key when no longer needed, using the command "../../bin/express-cli --aws-key-des ${keyName}"`
+    )
   }
-
-  const addKeyTasks = nodeIps.map(async (ip) => {
-    console.log(`📍 Adding ssh pubKey for ${keyName} to host ` + ip)
-    const command = `echo ${pubKey} >> ~/.ssh/authorized_keys`
-    await runSshCommand(ip, command, maxRetries)
-  })
-
-  await Promise.all(addKeyTasks)
-  console.log(`📍 Successfully added ${keyName} to all machines of the devnet`)
-  console.log(
-    `🔑 You can now share ${keyName}.pem with other devs - on a secure channel - to let them access the devnet`
-  )
-  console.log(
-    `🚨 Do not forget to destroy the key when no longer needed, using the command "../../bin/express-cli --aws-key-des ${keyName}"`
-  )
 }
--- src/express/commands/aws-keypair-destroy.js
+++ src/express/commands/aws-keypair-destroy.js
@@ -15,65 +15,96 @@ export async function awsKeypairDestroy(keyName) {
   const doc = await loadDevnetConfig(devnetType)
 
   const cloud = doc.cloud.toString()
-  
-  if (cloud == 'gcp'){
-    console.log('📍 Not supported for gcp at the moment')
-    return
-  }
 
-  console.log(`📍 Getting pubKey for ${keyName} ...`)
-  const output = shell.exec(
-    `aws ec2 describe-key-pairs --key-names ${keyName} --include-public-key --output json`
-  )
-  if (shell.error() !== null) {
-    console.log(`📍 Failed to get pubKey from ${keyName}`)
-    process.exit(1)
-  }
-  const pubKey = JSON.parse(output).KeyPairs[0].PublicKey.toString()
+  if (cloud == 'aws') {
+    console.log(`📍 Getting pubKey for ${keyName} ...`)
+    const output = shell.exec(
+      `aws ec2 describe-key-pairs --key-names ${keyName} --include-public-key --output json`
+    )
+    if (shell.error() !== null) {
+      console.log(`📍 Failed to get pubKey from ${keyName}`)
+      process.exit(1)
+    }
+    const pubKey = JSON.parse(output).KeyPairs[0].PublicKey.toString()
 
-  const totalHosts = []
-  const totalUsers = []
-  const nodeIps = []
-  if (doc.devnetBorHosts) {
-    totalHosts.push(...splitToArray(doc.devnetBorHosts.toString()))
-  }
-  if (doc.devnetErigonHosts) {
-    totalHosts.push(...splitToArray(doc.devnetErigonHosts.toString()))
-  }
+    const totalHosts = []
+    const totalUsers = []
+    const nodeIps = []
+    if (doc.devnetBorHosts) {
+      totalHosts.push(...splitToArray(doc.devnetBorHosts.toString()))
+    }
+    if (doc.devnetErigonHosts) {
+      totalHosts.push(...splitToArray(doc.devnetErigonHosts.toString()))
+    }
 
-  if (doc.devnetBorUsers) {
-    totalUsers.push(...splitToArray(doc.devnetBorUsers.toString()))
+    if (doc.devnetBorUsers) {
+      totalUsers.push(...splitToArray(doc.devnetBorUsers.toString()))
+    }
+    if (doc.devnetErigonUsers) {
+      totalUsers.push(...splitToArray(doc.devnetErigonUsers.toString()))
+    }
+    let ip
+
+    for (let i = 0; i < totalHosts.length; i++) {
+      ip = `${totalUsers[i]}@${totalHosts[i]}`
+      nodeIps.push(ip)
+    }
+
+    const removeKeyTasks = nodeIps.map(async (ip) => {
+      console.log(`📍 Removing ssh pubKey for ${keyName} from host ${ip} ...`)
+      const command = `sed -i 's,${pubKey}, ,' ~/.ssh/authorized_keys`
+      await runSshCommand(ip, command, maxRetries)
+    })
+
+    await Promise.all(removeKeyTasks)
+
+    console.log(`📍 Destroying aws remote key-pair for ${keyName} ...`)
+    shell.exec(`aws ec2 delete-key-pair --key-name ${keyName}`)
+    if (shell.error() !== null) {
+      console.log('📍Failed to destroy aws key-pair remotely')
+      process.exit(1)
+    }
+
+    console.log(`📍 Removing key-pair for ${keyName} locally ...`)
+    shell.exec(`rm ./${keyName}.pem`)
+    if (shell.error() !== null) {
+      console.log(`📍Failed to delete ${keyName}.pem locally`)
+    }
+
+    console.log(`📍 Key-pair ${keyName} destroyed successfully!`)
+  } else if (cloud == 'gcp') {
+    const user = doc.ethHostUser.toString()
+    const project = doc.instancesIds[0].split('/')[1].toString()
+    const zone = doc.instancesIds[0].split('/')[3].toString()
+    const instances = doc.instancesIds.map(x => x.split('/').at(-1)).toString().replace(/,/g, ' ').split(" ")
+
+    await Promise.all(instances.map(async (instance) => {
+      console.log(`📍 Getting pubKey for ${keyName} ...`)
+      const existing_pub_keys = shell.exec(
+        `gcloud compute instances describe ${instance} --project=${project} --zone=${zone} --format='value(metadata.ssh-keys)'`
+      )
+      const existing_ssh_keys = existing_pub_keys.split("\n");
+      const filtered_ssh_keys = existing_ssh_keys.filter(line => !line.trim().endsWith(keyName));
+      
+      if(existing_pub_keys.length != filtered_ssh_keys.length){
+        console.log(`📍 Failed to get pubKey from ${keyName}`)
+        process.exit(1)
+      } 
+      
+      const updated_ssh_keys = filtered_ssh_keys.join("\n");
+      shell.exec(
+        `gcloud compute instances add-metadata ${instance} --metadata ssh-keys="${updated_ssh_keys}" --project=${project} --zone=${zone}`
+      )
+    }
+    ))
+
+    console.log(`📍 Removing key-pair for ${keyName} locally ...`)
+    shell.exec(`rm ./${keyName}.pem`)
+    shell.exec(`rm ./${keyName}.pem.pub`)
+    if (shell.error() !== null) {
+      console.log(`📍Failed to delete ${keyName}.pem locally`)
+    }
+
+    console.log(`📍 Key-pair ${keyName} destroyed successfully!`)
   }
-  if (doc.devnetErigonUsers) {
-    totalUsers.push(...splitToArray(doc.devnetErigonUsers.toString()))
-  }
-  let ip
-
-  for (let i = 0; i < totalHosts.length; i++) {
-    ip = `${totalUsers[i]}@${totalHosts[i]}`
-    nodeIps.push(ip)
-  }
-
-  const removeKeyTasks = nodeIps.map(async (ip) => {
-    console.log(`📍 Removing ssh pubKey for ${keyName} from host ${ip} ...`)
-    const command = `sed -i 's,${pubKey}, ,' ~/.ssh/authorized_keys`
-    await runSshCommand(ip, command, maxRetries)
-  })
-
-  await Promise.all(removeKeyTasks)
-
-  console.log(`📍 Destroying aws remote key-pair for ${keyName} ...`)
-  shell.exec(`aws ec2 delete-key-pair --key-name ${keyName}`)
-  if (shell.error() !== null) {
-    console.log('📍Failed to destroy aws key-pair remotely')
-    process.exit(1)
-  }
-
-  console.log(`📍 Removing key-pair for ${keyName} locally ...`)
-  shell.exec(`rm ./${keyName}.pem`)
-  if (shell.error() !== null) {
-    console.log(`📍Failed to delete ${keyName}.pem locally`)
-  }
-
-  console.log(`📍 Key-pair ${keyName} destroyed successfully!`)
 }
