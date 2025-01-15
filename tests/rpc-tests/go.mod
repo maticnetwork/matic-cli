2d4e71db335b62c1d01d0fb12be4ee10176088df
Lucca Martins <lucca_martins30@yahoo.com.br>
2025-01-15 16:19:40 -0300

    including more rpc tests

--- tests/rpc-tests/go.mod
+++ tests/rpc-tests/go.mod
@@ -2,21 +2,30 @@ module rpc-tests
 
 go 1.22.1
 
+require github.com/ethereum/go-ethereum v1.14.12
+
 require (
 	github.com/bits-and-blooms/bitset v1.13.0 // indirect
+	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.3 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.2 // indirect
+	github.com/btcsuite/btcutil v1.0.3-0.20201208143702-a53e38424cce // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240223125850-b1e8a79f509c // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
-	github.com/ethereum/go-ethereum v1.14.12 // indirect
 	github.com/ethereum/go-verkle v0.1.1-0.20240829091221-dffa7562dbe9 // indirect
 	github.com/holiman/uint256 v1.3.1 // indirect
+	github.com/miguelmota/go-ethereum-hdwallet v0.1.2 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/supranational/blst v0.3.13 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/sync v0.7.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 	rsc.io/tmplfunc v0.0.3 // indirect
 )
+
+replace github.com/ethereum/go-ethereum => github.com/maticnetwork/bor v1.5.4
--- tests/rpc-tests/main.go
+++ tests/rpc-tests/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"bytes"
+	"crypto/ecdsa"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"math/big"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/consensus/bor/valset"
+	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 )
 
 // Request represents the JSON-RPC request payload.
@@ -39,11 +46,13 @@ type ResponseMap struct {
 	chainId                *big.Int
 	mostRecentBlock        *big.Int
 	currentProposerAddress common.Address
+	accounts               Accounts
+	gasPrice               *big.Int
 }
 
 type TestCase struct {
 	Key            string
-	PrepareRequest func(*ResponseMap) Request
+	PrepareRequest func(*ResponseMap) (*Request, error)
 	HandleResponse func(*ResponseMap, Response) error
 }
 
@@ -53,18 +62,36 @@ type FailedTestCase struct {
 	Key string
 }
 
+var (
+	rpcURL   = flag.String("rpc-url", "", "RPC Url to be tested")
+	mnemonic = flag.String("mnemonic", "", "mnemonic to be used on transactions")
+)
+
 func main() {
+	flag.Parse()
+	if *mnemonic == "" {
+		fmt.Println("Invalid mnemonic flag")
+		return
+	}
+	if *rpcURL == "" {
+		fmt.Println("Invalid rpcURL flag")
+		return
+	}
+
 	// Ethereum node RPC endpoint
-	rpcURL := "https://rpc-amoy.polygon.technology"
 	mapTestCases := testCasesToMap(testCases)
 	responseMap := ResponseMap{}
 	mapRequestIdToKey := make(map[int]string)
 	failedTestCases := []FailedTestCase{}
-
+	responseMap.accounts = generateAccountsUsingMnemonic(*mnemonic, 10)
 	testCaseBatches := []BatchTestCase{
 		[]TestCase{
 			mapTestCases["eth_chainId"],
 			mapTestCases["eth_blockNumber"],
+			mapTestCases["eth_getTransactionCount"],
+			mapTestCases["eth_getBalance"],
+			mapTestCases["eth_gasPrice"],
+			mapTestCases["eth_feeHistory"],
 			mapTestCases["bor_getCurrentProposer"],
 			mapTestCases["bor_getCurrentValidators"],
 		},
@@ -84,15 +111,23 @@ func main() {
 	for _, testCaseBatch := range testCaseBatches {
 		// Preparing Request
 		requests := make([]Request, 0)
+		countTestCases += len(testCaseBatch)
 		for _, testCase := range testCaseBatch {
-			req := testCase.PrepareRequest(&responseMap)
+			req, err := testCase.PrepareRequest(&responseMap)
+			if err != nil {
+				failedTestCases = append(failedTestCases, FailedTestCase{Key: testCase.Key, Err: err})
+				continue
+			}
+			if req == nil {
+				continue
+			}
+
 			mapRequestIdToKey[req.ID] = testCase.Key
 
-			requests = append(requests, req)
-			countTestCases++
+			requests = append(requests, *req)
 		}
 
-		responses, err := CallEthereumRPC(requests, rpcURL)
+		responses, err := CallEthereumRPC(requests, *rpcURL)
 		if err != nil {
 			fmt.Printf("Error while calling Ethereum RPC: %v\n", err)
 		}
@@ -108,11 +143,14 @@ func main() {
 		}
 	}
 
-	for _, failedTestCase := range failedTestCases {
-		fmt.Printf("failed test case: %s | err:%s", failedTestCase.Key, failedTestCase.Err)
-	}
-
 	fmt.Printf("All Tests Executed | Success: (%d/%d) | Duration: %s\n", countTestCases-len(failedTestCases), countTestCases, time.Since(timeStart))
+
+	if len(failedTestCases) > 0 {
+		fmt.Println("Failed Tests Cases:")
+	}
+	for _, failedTestCase := range failedTestCases {
+		fmt.Printf("\tkey: %s | err:%s\n", failedTestCase.Key, failedTestCase.Err)
+	}
 }
 
 func testCasesToMap(testCases []TestCase) map[string]TestCase {
@@ -156,20 +194,13 @@ func CallEthereumRPC(reqPayload []Request, rpcURL string) ([]Response, error) {
 }
 
 // NewRequest creates a new Request with Jsonrpc set to "2.0" and other fields given as parameters.
-func NewRequest(method string, params interface{}) Request {
-	return Request{
+func NewRequest(method string, params interface{}) *Request {
+	return &Request{
 		Jsonrpc: "2.0",
 		Method:  method,
 		Params:  params,
 		ID:      rand.Int(),
 	}
-}
-
-type Validator struct {
-	ID               uint64         `json:"ID"`
-	Address          common.Address `json:"signer"`
-	VotingPower      int64          `json:"power"`
-	ProposerPriority int64          `json:"accum"`
 }
 
 func parseResponse[T any](raw json.RawMessage) (*T, error) {
@@ -181,63 +212,120 @@ func parseResponse[T any](raw json.RawMessage) (*T, error) {
 	return &response, nil
 }
 
+type Account struct {
+	key   *ecdsa.PrivateKey
+	addr  common.Address
+	nonce *big.Int
+}
+type Accounts []Account
+type feeHistoryResult struct {
+	OldestBlock      *hexutil.Big     `json:"oldestBlock"`
+	Reward           [][]*hexutil.Big `json:"reward,omitempty"`
+	BaseFee          []*hexutil.Big   `json:"baseFeePerGas,omitempty"`
+	GasUsedRatio     []float64        `json:"gasUsedRatio"`
+	BlobBaseFee      []*hexutil.Big   `json:"baseFeePerBlobGas,omitempty"`
+	BlobGasUsedRatio []float64        `json:"blobGasUsedRatio,omitempty"`
+}
+
+func generateAccountsUsingMnemonic(MNEMONIC string, N int) (accounts Accounts) {
+	wallet, err := hdwallet.NewFromMnemonic(MNEMONIC)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 1; i <= N; i++ {
+		var dpath = "m/44'/60'/0'/0/" + strconv.Itoa(i)
+		path := hdwallet.MustParseDerivationPath(dpath)
+		account, err := wallet.Derive(path, false)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		privKey, err := wallet.PrivateKey(account)
+		if err != nil {
+			log.Fatal(err)
+		}
+		accounts = append(accounts, Account{key: privKey, addr: account.Address})
+	}
+	return accounts
+}
+
+func hexStringToBigInt(hexString string) (*big.Int, error) {
+	// Remove the "0x" prefix if present
+	if len(hexString) >= 2 && hexString[:2] == "0x" {
+		hexString = hexString[2:]
+	}
+
+	// Convert the hex string to an integer using big.Int
+	intValue, success := new(big.Int).SetString(hexString, 16)
+	if !success {
+		return nil, fmt.Errorf("error converting hex string to integer")
+	}
+	return intValue, nil
+}
+
 var testCases = []TestCase{
 	{
 		Key: "eth_chainId",
-		PrepareRequest: func(rm *ResponseMap) Request {
-			return NewRequest("eth_chainId", []interface{}{})
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("eth_chainId", []interface{}{}), nil
 		},
 		HandleResponse: func(rm *ResponseMap, resp Response) error {
 			parsed, err := parseResponse[string](resp.Result)
 			if err != nil {
 				return err
 			}
-			hexString := *parsed
-
-			// Remove the "0x" prefix if present
-			if len(hexString) >= 2 && hexString[:2] == "0x" {
-				hexString = hexString[2:]
+			intValue, err := hexStringToBigInt(*parsed)
+			if err != nil {
+				return err
 			}
 
-			// Convert the hex string to an integer using big.Int
-			intValue, success := new(big.Int).SetString(hexString, 16)
-			if !success {
-				return fmt.Errorf("error converting hex string to integer")
-			}
 			(*rm).chainId = intValue
 			return nil
 		},
 	},
 	{
 		Key: "eth_blockNumber",
-		PrepareRequest: func(rm *ResponseMap) Request {
-			return NewRequest("eth_blockNumber", []interface{}{})
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("eth_blockNumber", []interface{}{}), nil
 		},
 		HandleResponse: func(rm *ResponseMap, resp Response) error {
 			parsed, err := parseResponse[string](resp.Result)
 			if err != nil {
 				return err
 			}
-			hexString := *parsed
-
-			// Remove the "0x" prefix if present
-			if len(hexString) >= 2 && hexString[:2] == "0x" {
-				hexString = hexString[2:]
-			}
-
-			// Convert the hex string to an integer using big.Int
-			intValue, success := new(big.Int).SetString(hexString, 16)
-			if !success {
-				return fmt.Errorf("error converting hex string to integer")
+			intValue, err := hexStringToBigInt(*parsed)
+			if err != nil {
+				return err
 			}
 			(*rm).mostRecentBlock = intValue
 			return nil
 		},
 	},
 	{
+		Key: "eth_getTransactionCount",
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("eth_getTransactionCount", []interface{}{(*rm).accounts[0].addr, "latest"}), nil
+		},
+		HandleResponse: func(rm *ResponseMap, resp Response) error {
+			parsed, err := parseResponse[string](resp.Result)
+			if err != nil {
+				return err
+			}
+			intValue, err := hexStringToBigInt(*parsed)
+			if err != nil {
+				return err
+			}
+
+			// set nonce for accounts[0]
+			(*rm).accounts[0].nonce = intValue
+			return nil
+		},
+	},
+	{
 		Key: "bor_getCurrentProposer",
-		PrepareRequest: func(rm *ResponseMap) Request {
-			return NewRequest("bor_getCurrentProposer", []interface{}{})
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("bor_getCurrentProposer", []interface{}{}), nil
 		},
 		HandleResponse: func(rm *ResponseMap, resp Response) error {
 			currentProposerAddress, err := parseResponse[common.Address](resp.Result)
@@ -253,11 +341,11 @@ var testCases = []TestCase{
 	},
 	{
 		Key: "bor_getCurrentValidators",
-		PrepareRequest: func(rm *ResponseMap) Request {
-			return NewRequest("bor_getCurrentValidators", []interface{}{})
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("bor_getCurrentValidators", []interface{}{}), nil
 		},
 		HandleResponse: func(rm *ResponseMap, resp Response) error {
-			validators, err := parseResponse[[]Validator](resp.Result)
+			validators, err := parseResponse[[]valset.Validator](resp.Result)
 			if err != nil {
 				return err
 			}
@@ -270,8 +358,8 @@ var testCases = []TestCase{
 	},
 	{
 		Key: "eth_getBlockByNumber",
-		PrepareRequest: func(rm *ResponseMap) Request {
-			return NewRequest("eth_getBlockByNumber", []interface{}{fmt.Sprintf("0x%x", (*rm).mostRecentBlock), true})
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("eth_getBlockByNumber", []interface{}{fmt.Sprintf("0x%x", (*rm).mostRecentBlock), true}), nil
 		},
 		HandleResponse: func(rm *ResponseMap, resp Response) error {
 			block, err := parseResponse[map[string]interface{}](resp.Result)
@@ -280,6 +368,100 @@ var testCases = []TestCase{
 			}
 			if block == nil {
 				return fmt.Errorf("block not found")
+			}
+			return nil
+		},
+	},
+	{
+		Key: "eth_getBalance",
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("eth_getBalance", []interface{}{(*rm).accounts[0].addr, "latest"}), nil
+		},
+		HandleResponse: func(rm *ResponseMap, resp Response) error {
+			parsed, err := parseResponse[string](resp.Result)
+			if err != nil {
+				return err
+			}
+			balance, err := hexStringToBigInt(*parsed)
+			if err != nil {
+				return err
+			}
+
+			if (*balance).Cmp(big.NewInt(0)) <= 0 {
+				return fmt.Errorf("balance must be greater than 0")
+			}
+			return nil
+		},
+	},
+	{
+		Key: "eth_gasPrice",
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("eth_gasPrice", []interface{}{}), nil
+		},
+		HandleResponse: func(rm *ResponseMap, resp Response) error {
+			parsed, err := parseResponse[string](resp.Result)
+			if err != nil {
+				return err
+			}
+			gasPrice, err := hexStringToBigInt(*parsed)
+			if err != nil {
+				return err
+			}
+
+			if (*gasPrice).Cmp(big.NewInt(0)) <= 0 {
+				return fmt.Errorf("gas price must be greater than 0")
+			}
+			(*rm).gasPrice = gasPrice
+			return nil
+		},
+	},
+	{
+		Key: "eth_feeHistory",
+		PrepareRequest: func(rm *ResponseMap) (*Request, error) {
+			return NewRequest("eth_feeHistory", []interface{}{4, "latest", []int{25, 75}}), nil
+		},
+		HandleResponse: func(rm *ResponseMap, resp Response) error {
+			feeHistory, err := parseResponse[feeHistoryResult](resp.Result)
+			if err != nil {
+				return err
+			}
+			if (*big.Int)(feeHistory.OldestBlock).Cmp(big.NewInt(0)) <= 0 {
+				return fmt.Errorf("invalid oldest block: %s", (*big.Int)(feeHistory.OldestBlock).String())
+			}
+
+			// Check if Reward, BaseFee, and GasUsedRatio slices have consistent lengths
+			expectedLength := len(feeHistory.Reward)
+			if len(feeHistory.BaseFee) != expectedLength+1 {
+				return fmt.Errorf("BaseFee length must be Reward length + 1: got %d, expected %d", len(feeHistory.BaseFee), expectedLength+1)
+			}
+			if len(feeHistory.GasUsedRatio) != expectedLength {
+				return fmt.Errorf("GasUsedRatio length must match Reward length: got %d, expected %d", len(feeHistory.GasUsedRatio), expectedLength)
+			}
+
+			// Validate each BaseFee value is positive
+			for i, fee := range feeHistory.BaseFee {
+				if (*big.Int)(fee).Cmp(big.NewInt(0)) <= 0 {
+					return fmt.Errorf("invalid BaseFeePerGas at index %d: %s", i, (*big.Int)(fee).String())
+				}
+			}
+
+			// Validate GasUsedRatio values are within [0, 1]
+			for i, ratio := range feeHistory.GasUsedRatio {
+				if ratio < 0 || ratio > 1 {
+					return fmt.Errorf("invalid GasUsedRatio at index %d: %f", i, ratio)
+				}
+			}
+
+			// Validate Reward values are non-empty and positive
+			for i, rewards := range feeHistory.Reward {
+				if len(rewards) == 0 {
+					return fmt.Errorf("empty Reward slice at index %d", i)
+				}
+				for j, reward := range rewards {
+					if (*big.Int)(reward).Cmp(big.NewInt(0)) < 0 {
+						return fmt.Errorf("negative Reward value at Reward[%d][%d]: %s", i, j, (*big.Int)(reward).String())
+					}
+				}
 			}
 			return nil
 		},
