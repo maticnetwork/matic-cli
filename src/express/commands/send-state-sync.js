cfe30ae6d44ffb174faa66ba00f59f3305d5f063
Arya Lanjewar <102943033+AryaLanjewar3005@users.noreply.github.com>
2025-01-24 16:17:25 +0530

    fixed send-state-sync command

--- src/express/commands/send-state-sync.js
+++ src/express/commands/send-state-sync.js
@@ -41,10 +41,8 @@ export async function sendStateSyncTx() {
 
   console.log('📍Sending StateSync Tx')
   //const command = `cd ~/matic-cli/devnet/code/contracts && npm run truffle exec scripts/deposit.js -- --network development ${MaticToken} 100000000000000000000`
-  const icommand = 'cd ~/matic-cli/devnet/code/pos-contracts && export PATH="$HOME/.foundry/bin:$PATH" && forge init'
-  await runSshCommand(`${doc.ethHostUser}@${machine0}`, icommand, maxRetries)
 
-  const command = `export PATH="$HOME/.foundry/bin:$PATH" && forge script ~/matic-cli/devnet/code/pos-contracts/scripts/matic-cli-scripts/Deposit.s.sol:MaticDeposit --rpc-url http://localhost:9545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast --sig "run(address,address,uint256)" 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 ${MaticToken} 100000000000000000000`
+  const command = `export PATH="$HOME/.foundry/bin:$PATH" && cd ~/matic-cli/devnet/code/pos-contracts && forge script scripts/matic-cli-scripts/Deposit.s.sol:MaticDeposit --rpc-url http://localhost:9545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast --sig "run(address,address,uint256)" 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266 ${MaticToken} 100000000000000000000`
   await runSshCommand(`${doc.ethHostUser}@${machine0}`, command, maxRetries)
 
 
