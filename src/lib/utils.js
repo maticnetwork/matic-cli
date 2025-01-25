76524c1757ab3adbdc640215aa2501f6fd859676
Arya Lanjewar <102943033+AryaLanjewar3005@users.noreply.github.com>
2025-01-25 15:31:55 +0530

    mnemonics fix 02

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -103,7 +103,7 @@ export function createAccountsFromMnemonics(mnemonics, totalAccounts) {
   const accounts = []
   for(let i = 0; i < totalAccounts; i++) {
     //const account = new ethers.HDNodeWallet(ethers.Mnemonic.fromPhrase(mnemonics) , `m/44'/60'/0'/0/${i}`)
-    const mn = ethers.Mnemonic(mnemonics);
+    const mn = new ethers.Mnemonic(mnemonics);
     const account = new ethers.HDNodeWallet.fromMnemonic(mn, `m/44'/60'/0'/0/${i}`);
 
     accounts.push(account)
