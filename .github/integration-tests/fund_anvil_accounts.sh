f32d7e3271b03ac699fc86aa901ea8a02d85a731
kamuikatsurgi <shahkrishang11@gmail.com>
2025-02-18 16:27:32 +0530

    fix: add 0x

--- .github/integration-tests/fund_anvil_accounts.sh
+++ .github/integration-tests/fund_anvil_accounts.sh
@@ -11,7 +11,7 @@ rootChainWeb3="http://localhost:9545"
 for ((i = 1; i < signersLength; i++)); do
   to_address=$(echo "$signersDump" | jq -r ".[$i].address")
   from_address=$(echo "$signersDump" | jq -r ".[0].address")
-  txReceipt=$(curl $rootChainWeb3 -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"to":"'"$to_address"'","from":"'"$from_address"'","value":"8AC7230489E80000"}],"id":1}' -H "Content-Type: application/json")
+  txReceipt=$(curl $rootChainWeb3 -X POST --data '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"to":"'"$to_address"'","from":"'"$from_address"'","value":"0x8AC7230489E80000"}],"id":1}' -H "Content-Type: application/json")
   txHash=$(echo "$txReceipt" | jq -r '.result')
   echo "Funds transferred from $from_address to $to_address with txHash: $txHash"
 done
