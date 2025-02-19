7ef8e6f64d04330ccb46f21d258eea12b6be36af
Arya Lanjewar <102943033+AryaLanjewar3005@users.noreply.github.com>
2025-02-19 21:23:07 +0530

    minor account fix

--- src/setup/anvil/index.js
+++ src/setup/anvil/index.js
@@ -17,7 +17,7 @@ export class Anvil {
   constructor(config, options = {}) {
     this.config = config
     this.mnemonic = config.mnemonic
-    console.log(this.mnemonic)
+    this.deployerAccount = createAccountsFromMnemonics(this.mnemonic, 1)
     this.deployerPrivateKey = this.deployerAccount[0].privateKey
 
     this.dbName = options.dbName || 'anvil-db'
