086127c65de2cf4f7a3e3f00c80be25ebc4a1954
marcello33 <marcelloardizzone@hotmail.it>
2025-03-27 13:23:20 +0100

    disable bor_grpc by default

--- src/setup/heimdall/index.js
+++ src/setup/heimdall/index.js
@@ -313,7 +313,7 @@ export class Heimdall {
                 /bor_rpc_url[ ]*=[ ]*".*"/gi,
                 'bor_rpc_url = "http://localhost:8545"'
               )
-              .replace(/bor_grpc_flag[ ]*=[ ]*".*"/gi, 'bor_grpc_flag = "true"')
+              .replace(/bor_grpc_flag[ ]*=[ ]*".*"/gi, 'bor_grpc_flag = "false"')
               .replace(
                 /bor_grpc_url[ ]*=[ ]*".*"/gi,
                 'bor_grpc_url = "localhost:3131"'
