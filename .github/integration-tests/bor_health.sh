fc943ea2f54c80b8240f9d2aa41dbeef083e563e
marcello33 <marcelloardizzone@hotmail.it>
2025-05-15 11:38:05 +0200

    test fix docker

--- .github/integration-tests/bor_health.sh
+++ .github/integration-tests/bor_health.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
+IPC_PATH="/var/lib/bor/data/bor.ipc"
+
+# Wait for the IPC file to exist
+while ! docker exec bor0 test -S "$IPC_PATH"; do
+  echo "Waiting for $IPC_PATH to be created..."
+  sleep 5
+done
+
 while true; do
-    peers=$(docker exec bor0 bash -c "bor attach /var/lib/bor/data/bor.ipc -exec 'admin.peers'")
-    block=$(docker exec bor0 bash -c "bor attach /var/lib/bor/data/bor.ipc -exec 'eth.blockNumber'")
+    peers=$(docker exec bor0 bor attach "$IPC_PATH" --exec 'admin.peers')
+    block=$(docker exec bor0 bor attach "$IPC_PATH" --exec 'eth.blockNumber')
 
     if [[ -n "$peers" ]] && [[ -n "$block" ]]; then
         break
     fi
+    echo "Waiting for Bor to respond..."
+    sleep 5
 done
 
 echo "Peers: $peers"
