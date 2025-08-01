27d08dd83fa222bae2fac1adb9f588dc83ddcc96
Lucca Martins <lucca_martins30@yahoo.com.br>
2025-08-01 15:56:05 -0300

    write command

--- util-scripts/bor/backfill-state-sync-txs/db.go
+++ util-scripts/bor/backfill-state-sync-txs/db.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
 // WriteMissingStateSyncTransactions reads the missing StateSyncTxs from file and write on the data path
 func WriteMissingStateSyncTransactions(dataPath string, txFile string) {
-	// TODO: implement DebugReadKey
+	// Open the file for reading
+	file, err := os.Open(txFile)
+	if err != nil {
+		fmt.Errorf("failed to open file %s: %w", txFile, err)
+	}
+	defer file.Close()
+
+	// Read all bytes from the file
+	data, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Errorf("failed to read file %s: %w", txFile, err)
+	}
+
+	// Unmarshal JSON into the same struct type
+	var instructions []WriteInstruction
+	if err := json.Unmarshal(data, &instructions); err != nil {
+		fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
+
+	for _, instruction := range instructions {
+		DebugWriteKey(dataPath, instruction.Key, instruction.Value)
+	}
 }
